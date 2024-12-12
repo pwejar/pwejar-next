8ce90ee2822f8d7aa7e5aaf3348dadafcd96b770
Daniel Kimaiga <68325982+pwejar@users.noreply.github.com>
2024-12-12 07:12:44 +0300

    app hosting yaml corrected

--- app/firebase/clientApp.ts
+++ app/firebase/clientApp.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const clientCredentials = {
-    apiKey: process.env.FIREBASE_NEXT_API_KEY,
-    authDomain: process.env.FIREBASE_NEXT_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_NEXT_DATABASE_URL,
-    projectId: process.env.FIREBASE_NEXT_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_NEXT_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_NEXT_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_NEXT_APP_ID,
-    measurementId: process.env.FIREBASE_NEXT_MEASUREMENT_ID
+    apiKey: process.env.FIRE_NEXT_API_KEY,
+    authDomain: process.env.FIRE_NEXT_AUTH_DOMAIN,
+    databaseURL: process.env.FIRE_NEXT_DATABASE_URL,
+    projectId: process.env.FIRE_NEXT_PROJECT_ID,
+    storageBucket: process.env.FIRE_NEXT_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRE_NEXT_MESSAGING_SENDER_ID,
+    appId: process.env.FIRE_NEXT_APP_ID,
+    measurementId: process.env.FIRE_NEXT_MEASUREMENT_ID
 }
 const app = initializeApp(clientCredentials)
 const db = getFirestore(app,);
