56e2f79c7342941f9c3c672113e184f30a0eebc3
Daniel Kimaiga <68325982+pwejar@users.noreply.github.com>
2024-12-12 07:42:46 +0300

    dark button

--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,7 @@ export default async function Home() {
                 </ol> */}
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
                   <a
-                    className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+                    className="rounded-full border border-solid border-black/[1] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
                     href="https://www.youtube.com/@pwejar"
                     target="_blank"
                     rel="noopener noreferrer"
